eb183fc4ac405959bedc06c8b2e8a07c26fd7f72
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:10:53 +0300

    2020:12:04 00:10

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='npm-register-cli',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/npm-register']
